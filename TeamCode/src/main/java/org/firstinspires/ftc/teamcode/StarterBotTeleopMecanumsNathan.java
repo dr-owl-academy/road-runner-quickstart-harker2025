3e68c7f1b3584ccebde600683e1033274e66e561
SomeToast56 <nathantwatt@gmail.com>
2026-03-21 11:43:29 -0700

    Nathan Branch created and cleaned up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleopMecanumsNathan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleopMecanumsNathan.java
@@ -30,7 +30,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp(name = "StarterBotTeleopMecanumsNathan", group = "StarterBot")
 //@Disabled
 public class StarterBotTeleopMecanumsNathan extends OpMode {
-    final double FEED_TIME_SECONDS = 0.20; //The feeder servos run this long when a shot is requested.
+    final double FEED_TIME_SECONDS = 0.40; //The feeder servos run this long when a shot is requested.
     final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
     final double FULL_SPEED = 5250.0;
 
