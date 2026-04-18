85afaa587e87112815d20f1cdae8bf364c892745
tamah <adrianlucastao@gmail.com>
2026-04-18 12:00:02 -0700

    Adrian pinpoint x2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdrianPinpoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdrianPinpoint.java
@@ -30,6 +30,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
+
 /*
  * This OpMode illustrates how to use the GoBildaPinpoint
  *
