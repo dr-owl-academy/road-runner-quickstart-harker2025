13c5e3aef0ad061e93b632f44416fba082145bf9
tamah <adrianlucastao@gmail.com>
2026-04-25 11:50:05 -0700

    pinpoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adrianpinpointlocolizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adrianpinpointlocolizer.java
@@ -39,7 +39,7 @@ public final class adrianpinpointlocolizer implements Localizer {
         driver.setOffsets(mmPerTick * PARAMS.parYTicks, mmPerTick * PARAMS.perpXTicks, DistanceUnit.MM);
 
         // TODO: reverse encoder directions if needed
-        initialParDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+        initialParDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
         initialPerpDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
 
         driver.setEncoderDirections(initialParDirection, initialPerpDirection);
