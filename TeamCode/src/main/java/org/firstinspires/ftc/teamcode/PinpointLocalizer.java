e5262c3f4e71aa1042cf2915917121529b29438d
fmafalcon9 <fma.falcon9@gmail.com>
2026-04-25 10:55:44 -0700

    hw april 18th

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
@@ -16,9 +16,9 @@ import java.util.Objects;
 @Config
 public final class PinpointLocalizer implements Localizer {
     public static class Params {
-        public double parYTicks = 1392.5742516; // y position of the parallel encoder (in tick units)
+        public double parYTicks = 862.0892748; // y position of the parallel encoder (in tick units)
         public double
-                perpXTicks = 490.72774104; // x position of the perpendicular encoder (in tick units)
+                perpXTicks = 530.5164768; // x position of the perpendicular encoder (in tick units)
 
 
 
